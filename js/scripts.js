fb5a378339d64d9e390a3ce9b5f5a746d577f2d4
Steve Zaske <github@zaske.com>
2017-06-02 10:51:09 -0700

    include initial JS code and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 // Business (or back-end) logic:
+var courses = ["design","cSharp","java", "php", "ruby"];
+
 var courseScore = function() {
 //math formula goes here
+var frontBackRange = parseInt($("#frontBack").val());
+var personalRange = parseInt($("#personal").val());
+var nerdLevelRange = parseInt($("#nerdLevel").val());
+var moneyRange = parseInt($("#money").val());
+var alignmentRange = parseInt($("#alignment").val());
+
+
 };
 
+
+
 // Everything below this line is user interface (or front-end) logic:
+
 var setRecommended = function(){
   //CSS code goes here
-  console.log("front:" + $("#frontBack").val() + ", money:" + $("#money").val())
+  courses.forEach(function(entry){
+    //calculate the course score
+    // if score is above zero highlight course box else fade it
+
+    courseScore();
+  });
 
   if ($("#frontBack").val() === "0") {
     $("#designCourse").addClass("recommended");
@@ -16,12 +33,10 @@ var setRecommended = function(){
 
 $(document).ready(function() {
 
-  var frontBackRange;
-  var moneyRange;
 
   $('input[type=range]').on('input', function () {
       //$(this).trigger('change');
-        Suggester();
+        setRecommended();
   });
 
 
